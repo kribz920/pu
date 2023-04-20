4d6157dd62e2d47c437bcfdfcebeb9428e5927a2
Chris Samar <35326209+kribz920@users.noreply.github.com>
2023-04-20 21:59:27 +0200

    comments done

--- comments/main.py
+++ comments/main.py
@@ -3,4 +3,14 @@ __winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
 __human_name__ = 'comments'
 
 # Add your code after this line
-cl
+
+# here 1e single line comment
+Freetext = "what ever i whant?" # the variable
+# here 2e single line comment
+print(Freetext) # print function
+
+"""
+wat een geneuzel met meerdere
+lijnen
+
+"""
