007853f03231c13aae049fbb1ed28df4f2918ecd
Chris Winc <samar.chris@gmail.com>
2023-05-10 13:32:10 +0200

    functions start

--- strings/main.py
+++ strings/main.py
@@ -3,26 +3,27 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-Player_1 = "Ruud Gullit"
-Player_2 = "Marco van Basten"
+player_1 = "Ruud Gullit"
+player_2 = "Marco van Basten"
 
 goal_0 = 32
 goal_1 = 54
 
-scorers = Player_1 + ' ' + str(goal_0) + "," + " " + Player_2 + ' ' + str(goal_1)
+scorers = player_1 + ' ' + str(goal_0) + "," + " " + player_2 + ' ' + str(goal_1)
 # print(scorers)
-report = Player_1 + " scored in the " + str(goal_0) + "nd minute" + f"\n{Player_2} scored in the {goal_1}th minute"
-
-print(report)
-player = "Ruud Gullit"
-first_name = player[player.find("R",0, 4): 4]
-# print(first_name)
-last_name_len = len(player[player.find("G"):])
-name_short = first_name[0] + ". " + player[player.find("G"):]
-
-chant_ruud = (first_name + "! ") * len(first_name)
-chant = chant_ruud.strip()
+report = player_1 + " scored in the " + str(goal_0) + "nd minute" + f"\n{player_2} scored in the {goal_1}th minute"
 
 
-good_chant = chant[-1] != " "
-# print(good_chant)
+player = player_1
+first_name = player[0:player.find(" ")]
+last_name_len = int(len(player[player.find(" "):]))-dsese     1
+
+
+name_short = first_name[0] + ". " + player[player.find(" "):]
+print(name_short)
+chant_player = (first_name + "! ") * len(first_name)
+chant = chant_player.strip()
+print(chant)
+
+good_chant = (chant[-1] != " ")
+print(good_chant)
